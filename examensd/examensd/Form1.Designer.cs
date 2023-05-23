38f83974c5b7e36224cf66daf1faabf0d1b7aae8
myauxcode <104797580+myauxcode@users.noreply.github.com>
2023-05-23 19:01:26 -0400

    Add files via upload

---
+++ examensd/examensd/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace examensd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.examenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.examenToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "89";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(287, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "55";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(418, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "0,1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(553, 64);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "3";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(55, 125);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(598, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(55, 161);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(598, 278);
+            this.textBox7.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.examenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(785, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // examenToolStripMenuItem
+            // 
+            this.examenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.examenToolStripMenuItem1});
+            this.examenToolStripMenuItem.Name = "examenToolStripMenuItem";
+            this.examenToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.examenToolStripMenuItem.Text = "Menu";
+            // 
+            // examenToolStripMenuItem1
+            // 
+            this.examenToolStripMenuItem1.Name = "examenToolStripMenuItem1";
+            this.examenToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+            this.examenToolStripMenuItem1.Text = "Examen";
+            this.examenToolStripMenuItem1.Click += new System.EventHandler(this.examenToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 525);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem examenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem examenToolStripMenuItem1;
+    }
+}
+
---
+++ examensd/examensd/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examensd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static double examensd(double x1, double a, double b, double vim, double m)
+        {
+            Boolean ban = true;
+            double i = 0;
+            double c = 0;
+            double i2 = -1;
+            double termino;
+            double vi = 2;
+            double s = 0;
+            double bas = 2;
+
+            vim = vim / m;
+
+            do
+            {
+                i = i + 1;
+                vi = vi + (i - 1);
+
+                c = a - b;//0
+                a = b;
+                b = c;
+
+                vim = vim * m;
+
+                i2 = i2 + 1;
+
+                termino = Math.Pow(((x1 + c) / (Math.Cos(vim) + Math.Pow(bas, i2))), (1 / vi));
+
+
+
+                if (ban == true)
+                {
+
+                    s = s - termino;
+                }
+                else
+                {
+
+                    s = s + termino;
+                }
+                ban = !ban;
+
+
+
+            } while (!(c == 0));
+
+
+            return s;
+
+        }
+
+
+        public static string examensdt(double x1, double a, double b, double vim, double m)
+        {
+            Boolean ban = true;
+            double i = 0;
+            double c = 0;
+            double i2 = -1;
+            string termino = "";
+            double vi = 2;
+            string s = "";
+            double bas = 2;
+
+            vim = vim / m;
+
+            do
+            {
+                i = i + 1;
+                vi = vi + (i - 1);
+
+                c = a - b;//0
+                a = b;
+                b = c;
+
+                vim = vim * m;
+
+                i2 = i2 + 1;
+
+                termino = "Math.Pow(((x1 + " + c + ") / (Math.Cos(" + vim + ") + Math.Pow(2, " + i2 + "))),  (1 / " + vi + "))";
+
+
+
+
+                if (ban == true)
+                {
+                    //restar
+                    s = s + "     -     " + termino;
+                }
+                else
+                {
+                    //sumar
+                    s = s + "      +   " + termino;
+                }
+                ban = !ban; // codigo para pinponear
+
+
+
+            } while (!(c == 0));
+
+
+            return s;
+
+        }
+        private void examenToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = string.Concat(examensd(double.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text), double.Parse(textBox4.Text), double.Parse(textBox5.Text)));
+            textBox7.Text = string.Concat(examensdt(double.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text), double.Parse(textBox4.Text), double.Parse(textBox5.Text)));
+
+
+        }
+    }
+}
